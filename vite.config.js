ad31be089dfdae6919a76342b8e039e72e030c83
rohan <rohan@claclaws.com>
2024-03-18 09:20:13 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,16 @@ export default defineConfig({
   plugins: [react()],
    server: {
     host: true,
-    // proxy:{
-    //   "/api":{
-    //     target:"https://chat-microservice-9gcv.onrender.com",
-      // changeOrigin: true,
-      // rewrite: path => path.replace(/^\/api/, '')
-    //   }
-    // }
     proxy:{
-      "/api":'http://localhost:3112'
+      "/api":{
+        target:"https://chat-microservice-9gcv.onrender.com",
+      changeOrigin: true,
+      rewrite: path => path.replace(/^\/api/, '')
+      }
     }
+    // proxy:{
+    //   "/api":'http://localhost:3112'
+    // }
    
    },
    build:{
